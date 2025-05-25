fdc006a52f342332655673118870160f1bdd892f
Owen <iamnytla@gmail.com>
2025-05-25 19:20:07 -0400

    Finished user posts API app with search functionality

--- user.js
+++ user.js
@@ -1,21 +1,22 @@
 const postListEl = document.querySelector('.post-list');
+const id = localStorage.getItem("id");
 
-async function main() {
-  const id = localStorage.getItem("id");
-  const posts = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
-  );
-  const postsData = await posts.json();
-  console.log(postsData);
-
-  postListEl.innerHTML = postsData.map((posts) => postsHTML(posts)).join('');
+async function onSearchChange(event) {
+    const id = event.target.value;
+    renderPosts(id);
 }
 
-function postsHTML(posts) {
+function postHTML(post) {
     return `<div class="post">
-    <div class="post__title">${posts.title}</div>
-    <p class="post__body">${posts.body}</p>
+    <div class="post__title">${post.title}</div>
+    <p class="post__body">${post.body}</p>
     </div>`
 }
 
-main();
+async function renderPosts(id) {
+  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+  const postsData = await posts.json();
+  postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
+}
+
+renderPosts(id);
