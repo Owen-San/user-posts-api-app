dc827e136d07af6fa91a76d4d9ee230582fc47ab
Owen <iamnytla@gmail.com>
2025-05-25 18:32:35 -0400

    Added user posts being shown by ID.

--- user.js
+++ user.js
@@ -1,8 +1,21 @@
+const postListEl = document.querySelector('.post-list');
+
 async function main() {
-    const id = localStorage.getItem("id")
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:${id}`)
-    const postsData = await posts.json();
-    console.log(postsData)
+  const id = localStorage.getItem("id");
+  const posts = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+  );
+  const postsData = await posts.json();
+  console.log(postsData);
+
+  postListEl.innerHTML = postsData.map((posts) => postsHTML(posts)).join('');
+}
+
+function postsHTML(posts) {
+    return `<div class="post">
+    <div class="post__title">${posts.title}</div>
+    <p class="post__body">${posts.body}</p>
+    </div>`
 }
 
 main();
